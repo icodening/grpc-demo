2ed702977254f454fc2a1c515ad14ab9f4476e5f
icodening <cleverbn96@gmail.com>
2022-09-17 15:22:44 +0800

    cancel

--- src/main/java/org/example/grpc/GrpcClient.java
+++ src/main/java/org/example/grpc/GrpcClient.java
@@ -1,6 +1,7 @@
 package org.example.grpc;
 
 import com.google.protobuf.ByteString;
+import io.grpc.ClientCall;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
@@ -8,6 +9,7 @@ import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import static org.example.grpc.ExchangerGrpc.getBiStreamMethod;
 import static org.example.grpc.ExchangerGrpc.newStub;
 
 /**
@@ -19,7 +21,9 @@ public class GrpcClient {
     public static void main(String[] args) throws IOException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 40000)
                 .usePlaintext().build();
-        StreamObserver<Message> requestObserver = newStub(channel).biStream(new StreamObserver<Message>() {
+        ExchangerGrpc.ExchangerStub stub = newStub(channel);
+        ClientCall<Message, Message> call = channel.newCall(getBiStreamMethod(), stub.getCallOptions());
+        StreamObserver<Message> requestObserver = stub.biStream(new StreamObserver<Message>() {
             @Override
             public void onNext(Message value) {
                 System.out.println("响应："
@@ -42,8 +46,10 @@ public class GrpcClient {
         requestObserver.onNext(req);
         requestObserver.onNext(req);
         requestObserver.onNext(req);
-//        call.cancel("hello ex", new RuntimeException("ex"));
+        call.cancel("hello ex", new RuntimeException("ex"));
         requestObserver.onCompleted();
+        requestObserver.onNext(req);
+        requestObserver.onNext(req);
         System.in.read();
     }
 }
